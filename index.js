fa43b8640a8ddb630423f8da633ca6460c715323
wachira <wachiraa954@gmail.com>
2022-06-14 11:30:01 +0300

    commit tree 218c9b9c64103bc64c0f5b594793ebbef0c238fc

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(done = "roller-skate"){
+    return `This Saturday, I want to ${done}!`
+}
+
+let mondayWork = function (done ="go to the office"){
+    return `This Monday, I will ${done}.`
+}
+
+let wrapAdjective = function(drop="*"){
+    return function (adject= "Good"){
+    return `You are ${drop}${adject}${drop}!`
+}
+}
